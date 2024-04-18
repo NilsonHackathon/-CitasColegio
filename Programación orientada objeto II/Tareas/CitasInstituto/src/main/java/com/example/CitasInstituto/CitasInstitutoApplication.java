eb64a963676aad7adf86da30f1cb031517b44175
Nilson Saballos A <nsaballos@uamv.edu.ni>
2024-04-17 18:20:36 -0600

    Subiendo cosas faltantes

---
+++ Programación orientada objeto II/Tareas/CitasInstituto/src/main/java/com/example/CitasInstituto/CitasInstitutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CitasInstituto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasInstitutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasInstitutoApplication.class, args);
+	}
+
+}
